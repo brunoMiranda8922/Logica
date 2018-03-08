ca9944b7f8f0228e4d4255a4a5156879e08fb054
Bruno Miranda <bruno.silva@catho.com>
2018-03-08 16:52:33 -0300

    Exercicio da loteria em python

---
+++ loteriaPython.py
@@ -0,0 +1,49 @@
+#   coding: utf-8
+import random
+
+
+
+def sorteia (num=5):
+    array_numero = []
+    i = 0
+    while i < 5:
+        numero = random.randint(0, 40) 
+        if numero not in array_numero:
+            array_numero.append(numero)
+            i = i + 1
+            print(array_numero)
+    return array_numero
+
+    
+def start():
+    numero_sorteado = sorteia(40)
+    chute = int(input("Chute um numero: "))
+    tentativa = 1
+    while (chute < 0 or chute > 40) and tentativa <3:
+        if tentativa == 3:
+            print("Tentativas esgotaram")
+            return
+        print("Número inválido")
+        chute = int(input("Chute novamente um numero: "))
+        tentativa = tentativa + 1
+
+    if chute in numero_sorteado:
+        print("Você acertou")
+    else:
+        print("Você errou o número")
+
+def Main():
+    start()
+    
+    
+
+Main()
+
+
+
+
+
+
+
+
+
