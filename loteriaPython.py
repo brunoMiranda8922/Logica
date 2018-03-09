ac47ec833599f02357757fbc89b6e7ee6a0348cc
Bruno Miranda <bruno.silva@catho.com>
2018-03-09 14:29:15 -0300

    Ordenando os elementos da array

--- loteriaPython.py
+++ loteriaPython.py
@@ -5,21 +5,27 @@ import random
 
 def sorteia (num=5):
     array_numero = []
+    
     i = 0
     while i < 5:
-        numero = random.randint(0, 40) 
+        numero = random.randint(0, 40)
         if numero not in array_numero:
             array_numero.append(numero)
+            array_numero.sort()
             i = i + 1
-            print(array_numero)
-    return array_numero
+            
 
+    for i in sorted(array_numero):
+        print(array_numero)
+    return array_numero
     
+
 def start():
     numero_sorteado = sorteia(40)
     chute = int(input("Chute um numero: "))
     tentativa = 1
     while (chute < 0 or chute > 40) and tentativa <3:
+        
         if tentativa == 3:
             print("Tentativas esgotaram")
             return
@@ -30,7 +36,9 @@ def start():
     if chute in numero_sorteado:
         print("Você acertou")
     else:
-        print("Você errou o número")
+        print("Você errou o número") 
+
+
 
 def Main():
     start()
